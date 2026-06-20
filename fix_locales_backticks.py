e0677a5b89a32a2d8da7f450c9b64221456e14fc
Flash11552 <sfrovli7@gmail.com>
2026-06-20 21:43:51 +0400

    Delete fix_locales_backticks.py
    Signed-off-by: Flash11552 <sfrovli7@gmail.com>

--- fix_locales_backticks.py
+++
@@ -1,8 +0,0 @@
-with open("/workspace/app-cej2tb5o4l4x/tasks/saski_bot/locales.py", "r", encoding="utf-8") as f:
-    text = f.read()
-
-text = text.replace("`{total_games}`", "{total_games}")
-
-with open("/workspace/app-cej2tb5o4l4x/tasks/saski_bot/locales.py", "w", encoding="utf-8") as f:
-    f.write(text)
-print("Removed backticks from total_games")
